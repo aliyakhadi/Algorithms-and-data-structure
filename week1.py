14fd0835ad6c855dede87bca0381f2ef150d0269
ValeevKarim <152394353+ValeevKarim@users.noreply.github.com>
2024-02-19 12:35:48 +0300

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,15 @@
+def primesum(self, A):
+    def isprime(n):
+        if n == 1:
+            return False
+        if n == 2:
+            return True
+        for i in range(2, round(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+    for i in range(2, A):
+        if isprime(i) and isprime(A-i):
+            return [i, A-i]
+
+
